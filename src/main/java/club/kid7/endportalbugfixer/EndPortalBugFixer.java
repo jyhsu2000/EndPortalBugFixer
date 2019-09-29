568f45dcdd89920a9ccfcf73f8bf3e202100daeb
arboriginal <1117167+arboriginal@users.noreply.github.com>
2019-09-29 21:10:48 +0200

    Improvements
    * Add a command to reload plugin config
    * Add the associated permission
    * Make the forbidden items configurable
    * Merge both class, as there is not much code
    * Refactor / improve performances
    * Handle cancelled events

--- src/main/java/club/kid7/endportalbugfixer/EndPortalBugFixer.java
+++ src/main/java/club/kid7/endportalbugfixer/EndPortalBugFixer.java
@@ -1,12 +1,68 @@
 package club.kid7.endportalbugfixer;
 
+import java.util.List;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class EndPortalBugFixer extends JavaPlugin {
+public final class EndPortalBugFixer extends JavaPlugin implements Listener {
+  private List<String> forbidden;
 
-    @Override
-    public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
+  // -- JavaPlugin methods --------------------------------------------------------------------------------------------
+
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    if (command.getName().equalsIgnoreCase("EndPortalBugFixer-reload")) {
+      reloadConfig();
+      sender.sendMessage("§a[EndPortalBugFixer] Configuration reloaded.");
+      return true;
     }
+
+    return super.onCommand(sender, command, label, args);
+  }
+
+  @Override
+  public void onDisable() {
+    super.onDisable();
+    HandlerList.unregisterAll((JavaPlugin) this);
+  }
+
+  @Override
+  public void onEnable() {
+    reloadConfig();
+    Bukkit.getPluginManager().registerEvents(this, this);
+  }
+
+  @Override
+  public void reloadConfig() {
+    super.reloadConfig();
+
+    saveDefaultConfig();
+    getConfig().options().copyDefaults(true);
+    saveConfig();
+
+    forbidden = getConfig().getStringList("forbidden");
+  }
+
+  // -- Listener methods ----------------------------------------------------------------------------------------------
+
+  @EventHandler
+  void onPlayerInteract(PlayerInteractEvent event) {
+    if (event.isCancelled() || event.getClickedBlock() == null || !event.getAction().equals(Action.RIGHT_CLICK_BLOCK)
+        || event.getPlayer().hasPermission("EndPortalBugFixer.bypass")
+        || !event.getClickedBlock().getRelative(event.getBlockFace()).getType().equals(Material.END_PORTAL))
+      return;
+
+    ItemStack item = event.getItem();
+
+    if (item != null && forbidden.contains(item.getType().toString())) event.setCancelled(true);
+  }
 }
